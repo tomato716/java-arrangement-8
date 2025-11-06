44dbbc623a6b8029a390459fb53d6ef1954a8d3f
JunSeok_Park <pjh203203@naaver.com>
2025-11-06 17:25:14 +0900

    feat: 숫자 입력 검증 구현

---
+++ src/main/java/arrangement/model/Validator.java
@@ -0,0 +1,25 @@
+package arrangement.model;
+
+public class Validator {
+    private static final int NUMBER_START = 1;
+    private static final int NUMBER_END = 4;
+
+    public void selectNumber(String input) {
+        int number = isDigit(input);
+        isCorrectNumberRange(number);
+    }
+
+    private void isCorrectNumberRange(int number) {
+        if (number < NUMBER_START || number > NUMBER_END) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 번호입니다.");
+        }
+    }
+
+    private int isDigit(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력하세요.");
+        }
+    }
+}
