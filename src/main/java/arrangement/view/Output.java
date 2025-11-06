cf0e78f61237bef6331e7971876852dbc913b3d9
JunSeok_Park <pjh203203@naaver.com>
2025-11-06 10:17:16 +0900

    feat: output view 구현

---
+++ src/main/java/arrangement/view/Output.java
@@ -0,0 +1,21 @@
+package arrangement.view;
+
+public class Output {
+
+    public void printMission() {
+        System.out.println("--- 미션 ---");
+        System.out.println("재고를 정리하세요.");
+        System.out.println("소고기 2, 돼지고기 3, 물 2, 술 5");
+    }
+
+    public void requestMeat() {
+        System.out.print("Meat: ");
+    }
+
+    public void printStart() {
+        System.out.println("번호를 선택해주세요.");
+        System.out.println("1. 재고 보기");
+        System.out.println("2. 미션 보기");
+
+    }
+}
