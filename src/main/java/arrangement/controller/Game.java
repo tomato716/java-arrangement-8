c3532beb2d54f406cd02999b74ef9ee5647a760b
JunSeok_Park <pjh203203@naaver.com>
2025-11-06 17:25:30 +0900

    feat: 게임 시작 메서드 구현

--- src/main/java/arrangement/controller/Game.java
+++ src/main/java/arrangement/controller/Game.java
@@ -1,19 +1,23 @@
 package arrangement.controller;
 
+import arrangement.model.Validator;
 import arrangement.view.Input;
 import arrangement.view.Output;
 
 public class Game {
 
-    private final Input input;
     private final Output output;
+    private final Validator validator;
 
-    public Game(Input input, Output output) {
-        this.input = input;
+    public Game(Output output, Validator validator) {
         this.output = output;
+        this.validator = validator;
     }
 
     public void play() {
+        output.printStart();
+        String input = Input.console();
+        validator.selectNumber(input);
 
     }
 }
