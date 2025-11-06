12384863771e3cc86af216efe4dd4d7796eb20f0
JunSeok_Park <pjh203203@naaver.com>
2025-11-06 10:16:58 +0900

    feat: 컨트롤러 구현

---
+++ src/main/java/arrangement/controller/Game.java
@@ -0,0 +1,19 @@
+package arrangement.controller;
+
+import arrangement.view.Input;
+import arrangement.view.Output;
+
+public class Game {
+
+    private final Input input;
+    private final Output output;
+
+    public Game() {
+        this.input = new Input();
+        this.output = new Output();
+    }
+
+    public void play() {
+
+    }
+}
