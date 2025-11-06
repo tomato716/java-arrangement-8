566c6c86fab14593c7bbc8f593b62b6945f9b40a
JunSeok_Park <pjh203203@naaver.com>
2025-11-06 10:17:35 +0900

    feat: Application 구현

---
+++ src/main/java/arrangement/Application.java
@@ -0,0 +1,10 @@
+package arrangement;
+
+import arrangement.view.Input;
+
+public class Application {
+
+    public static void main(String[] args) {
+        String console = Input.console();
+    }
+}
