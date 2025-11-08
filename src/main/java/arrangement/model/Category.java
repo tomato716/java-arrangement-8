ceb0caa3131913c33a172074a490a452f2832f9f
JunSeok_Park <pjh203203@naaver.com>
2025-11-08 11:21:37 +0900

    feat: 카테고리 멤버 변수 추가

--- src/main/java/arrangement/model/Category.java
+++ src/main/java/arrangement/model/Category.java
@@ -1,8 +1,19 @@
 package arrangement.model;
 
 public enum Category {
-    MEAT,
-    DRINK,
-    VEGETABLE,
-    FRUIT;
+    MEAT("고기 종류"),
+    DRINK("음료 종류"),
+    VEGETABLE("야채 종류"),
+    FRUIT("과일 종류");
+
+    private final String koreanName;
+
+    Category(String koreanName) {
+        this.koreanName = koreanName;
+    }
+
+    @Override
+    public String toString() {
+        return koreanName;
+    }
 }
