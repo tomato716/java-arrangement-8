4c1c9eab9c8cdf0719c99dfba38c07afa38f2607
JunSeok_Park <pjh203203@naaver.com>
2025-11-08 15:12:33 +0900

    feat: 옵션에 미션 의존성 주입

--- src/main/java/arrangement/controller/option/OptionSelector.java
+++ src/main/java/arrangement/controller/option/OptionSelector.java
@@ -1,6 +1,7 @@
 package arrangement.controller.option;
 
 import arrangement.model.Inventory;
+import arrangement.model.Mission;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,7 +10,7 @@ public class OptionSelector {
 
     public OptionSelector() {
         selector.add(new InventoryOption(new Inventory()));
-        selector.add(new MissionOption());
+        selector.add(new MissionOption(new Mission()));
     }
 
     public Option select(int number) {
