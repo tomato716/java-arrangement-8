24ff969fcec900543c771312f801b03eb57af336
JunSeok_Park <pjh203203@naaver.com>
2025-11-08 18:32:48 +0900

    refactor: 수입에 대한 클래스 외부로 이동

--- src/main/java/arrangement/controller/option/OptionSelector.java
+++ src/main/java/arrangement/controller/option/OptionSelector.java
@@ -1,18 +1,21 @@
 package arrangement.controller.option;
 
+import arrangement.model.GameState;
 import arrangement.model.Income;
 import arrangement.model.Inventory;
 import arrangement.model.Mission;
+import arrangement.model.Shutdown;
 import java.util.ArrayList;
 import java.util.List;
 
 public class OptionSelector {
     private final List<Option> selector = new ArrayList<>();
 
-    public OptionSelector() {
+    public OptionSelector(GameState gameState, Income income) {
         selector.add(new InventoryOption(new Inventory()));
         selector.add(new MissionOption(new Mission()));
-        selector.add(new IncomeOption(new Income()));
+        selector.add(new IncomeOption(income));
+        selector.add(new ShutdownOption(new Shutdown(income, gameState)));
     }
 
     public Option select(int number) {
