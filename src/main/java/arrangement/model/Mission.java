7d28c936a32d2b3fa73d50f6948661912e1eaa46
JunSeok_Park <pjh203203@naaver.com>
2025-11-08 15:12:02 +0900

    feat: 미션 클래스 구현

---
+++ src/main/java/arrangement/model/Mission.java
@@ -0,0 +1,39 @@
+package arrangement.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mission {
+    private final List<String> mission = new ArrayList<>();
+
+    public Mission() {
+        mission.add("소고기");
+        mission.add("콜라");
+        mission.add("상추");
+        mission.add("사이다");
+        mission.add("사과");
+        mission.add("돼지고기");
+        mission.add("깻잎");
+        mission.add("키위");
+        mission.add("파인애플");
+    }
+
+    public void showMission() {
+        System.out.println(mission);
+    }
+
+    public void removeMission(String input) {
+        validate(input);
+        mission.remove(input);
+    }
+
+    private void validate(String input) {
+        if (!mission.contains(input)) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다.");
+        }
+    }
+
+    public int size() {
+        return mission.size();
+    }
+}
