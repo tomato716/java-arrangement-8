e4765d026a6af0b80341334c4a2bbcd488b136a9
JunSeok_Park <pjh203203@naaver.com>
2025-11-08 15:12:16 +0900

    feat: 미션 옵션 기능 구현

--- src/main/java/arrangement/controller/option/MissionOption.java
+++ src/main/java/arrangement/controller/option/MissionOption.java
@@ -1,8 +1,32 @@
 package arrangement.controller.option;
 
+import arrangement.model.Mission;
+import arrangement.view.Input;
+
 public class MissionOption implements Option {
+    private final Mission mission;
+
+    public MissionOption(Mission mission) {
+        this.mission = mission;
+    }
+
     @Override
     public void play() {
+        System.out.println("--- 미션 ---");
 
+        removeMission();
+    }
+
+    private void removeMission() {
+        while (mission.size() != 0) {
+            try {
+                mission.showMission();
+                System.out.println("카테고리 별로 정리하세요.");
+                String input = Input.console();
+                mission.removeMission(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
