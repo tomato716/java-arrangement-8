a065f9a5fce211093f2f768b04fc9306c50646f7
JunSeok_Park <pjh203203@naaver.com>
2025-11-16 15:08:16 +0900

    feat(Category): 카테고리별 item 목록 추가

--- src/main/java/arrangement/model/Category.java
+++ src/main/java/arrangement/model/Category.java
@@ -1,15 +1,19 @@
 package arrangement.model;
 
+import java.util.List;
+
 public enum Category {
-    MEAT("🥩 고기 종류"),
-    DRINK("🥤 음료 종류"),
-    VEGETABLE("🥬 야채 종류"),
-    FRUIT("🍎 과일 종류");
+    MEAT("🥩 고기 종류", List.of("소고기", "돼지고기", "양고기")),
+    DRINK("🥤 음료 종류", List.of("콜라", "사이다", "삼다수")),
+    VEGETABLE("🥬 야채 종류", List.of("상추", "깻잎", "배추")),
+    FRUIT("🍎 과일 종류", List.of("사과", "키위", "파인애플"));
 
     private final String koreanName;
+    private final List<String> items;
 
-    Category(String koreanName) {
+    Category(String koreanName, List<String> items) {
         this.koreanName = koreanName;
+        this.items = items;
     }
 
     @Override
