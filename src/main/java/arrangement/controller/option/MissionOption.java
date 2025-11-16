e448b555a5357340eecf01e11a991180b61e4e2e
JunSeok_Park <pjh203203@naaver.com>
2025-11-16 15:04:47 +0900

    feat: 입력 값을 검증하여 카테고리 제거 기능 구현

--- src/main/java/arrangement/controller/option/MissionOption.java
+++ src/main/java/arrangement/controller/option/MissionOption.java
@@ -1,26 +1,24 @@
 package arrangement.controller.option;
 
+import arrangement.model.Category;
+import arrangement.model.Inventory;
 import arrangement.model.Mission;
-import arrangement.model.Validator;
 import arrangement.view.Input;
+import java.util.Arrays;
+import java.util.List;
 
 public class MissionOption implements Option {
     private final Mission mission;
-    private final Validator validator;
+    private final Inventory inventory;
 
-    public MissionOption(Mission mission, Validator validator) {
+    public MissionOption(Mission mission, Inventory inventory) {
         this.mission = mission;
-        this.validator = validator;
+        this.inventory = inventory;
     }
 
     @Override
     public void play() {
         System.out.println("--- 미션 ---");
-
-        removeMission();
-    }
-
-    private void removeMission() {
         mission.createMission();
 
         while (mission.itemCount() != 0) {
@@ -29,11 +27,56 @@ public class MissionOption implements Option {
                 System.out.println("카테고리 별로 정리하세요.");
                 System.out.println("입력은 {카테고리} - {정리할 물품},{정리할 물품}으로 입력해주세요.");
                 String input = Input.console();
-                String[] strings = validator.splitCategoryItem(input);
-                mission.removeMission(input);
+                tryRemoveMission(input);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
+        }
+    }
+
+    private void tryRemoveMission(String input) {
+        List<String> categoryAndItems = splitCategoryItem(input);
+
+        Category category = validateCategory(categoryAndItems.getFirst());
+        List<String> items = splitInput(categoryAndItems.getLast(), ",");
+        validateRemoveItems(category, items);
+
+    }
+
+    private void validateRemoveItems(Category category, List<String> inputItems) {
+        for (String item : inputItems) {
+            if (!category.includeItem(item)) {
+                throw new IllegalArgumentException("[ERROR] 아이템을 잘못 입력했습니다.");
+            }
+            mission.removeMission(item);
+            inventory.addItem(category, item);
+        }
+    }
+
+    private List<String> splitInput(String input, String delimiter) {
+        return Arrays.stream(input.split(delimiter, -1))
+                .map(String::strip)
+                .toList();
+    }
+
+    private Category validateCategory(String category) {
+        Category name = Category.includeCategory(category);
+
+        if (name == null) {
+            throw new IllegalArgumentException("[ERROR] 카테고리는 MEAT, DRINK, VEGETABLE, FRUIT만 입력 가능합니다.");
+        }
+
+        return name;
+    }
+
+    private List<String> splitCategoryItem(String input) {
+        isHaveDash(input);
+        return splitInput(input, "-");
+    }
+
+    private void isHaveDash(String input) {
+        if (!input.contains("-")) {
+            throw new IllegalArgumentException("[ERROR] 잘못입력하셨습니다.");
         }
     }
 }
