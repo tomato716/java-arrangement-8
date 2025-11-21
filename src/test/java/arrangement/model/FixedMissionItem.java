584bfc85f673eab5039b8092eedf23da2aa7907c
JunSeok_Park <pjh203203@naaver.com>
2025-11-21 19:10:00 +0900

    test: 테스트하기 위한 고정된 미션 클래스 구현

---
+++ src/test/java/arrangement/model/FixedMissionItem.java
@@ -0,0 +1,12 @@
+package arrangement.model;
+
+import java.util.List;
+
+public class FixedMissionItem extends MissionItem {
+
+    @Override
+    public List<String> create() {
+
+        return List.of("양고기", "돼지고기", "소고기", "사과", "키위");
+    }
+}
