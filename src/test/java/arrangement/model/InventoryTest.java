b9a195a5827c51dd9a01a7cb3d87b439f09454d5
JunSeok_Park <pjh203203@naaver.com>
2025-11-21 15:37:29 +0900

    test: 재고를 처음 만들면 카테고리 별 아이템 목록이 0으로 초기화되는지 테스트 구현

--- src/test/java/arrangement/model/InventoryTest.java
+++ src/test/java/arrangement/model/InventoryTest.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class InventoryTest {
@@ -26,5 +27,21 @@ public class InventoryTest {
                 .hasSize(4)
                 .isNotEmpty()
                 .containsKey(category);
+    }
+
+    @DisplayName("재고를 생성하면 0으로 초기화되어 생성하는지 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "MEAT,양고기", "MEAT,소고기", "MEAT,돼지고기",
+            "DRINK,삼다수", "DRINK,사이다", "DRINK,콜라",
+            "FRUIT,파인애플", "FRUIT,사과", "FRUIT,키위",
+            "VEGETABLE,배추", "VEGETABLE,상추", "VEGETABLE,깻잎"
+    })
+    void initialInventoryItems(Category category, String koreanName) {
+        Map<Category, Map<String, Integer>> products = inventory.getProducts();
+
+        assertThat(products.get(category))
+                .containsOnlyKeys(category.getItems())
+                .containsEntry(koreanName, 0);
     }
 }
