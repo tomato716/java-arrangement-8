823c03f4947209f927b220e6a46587f5ab506e4f
JunSeok_Park <pjh203203@naaver.com>
2025-11-21 21:05:55 +0900

    refactor: 종료 검증은 스스로 책임을 갖도록 검증 책임 이동

--- src/test/java/arrangement/model/ShutdownTest.java
+++ src/test/java/arrangement/model/ShutdownTest.java
@@ -9,16 +9,14 @@ import org.junit.jupiter.api.Test;
 public class ShutdownTest {
     private Income income;
     private GameState gameState;
-    private Validator validator;
     private Shutdown shutdown;
 
     @BeforeEach
     void reset() {
         income = new Income();
         gameState = new GameState();
-        validator = new Validator();
 
-        shutdown = new Shutdown(income, gameState, validator);
+        shutdown = new Shutdown(income, gameState);
     }
 
     @DisplayName("소득이 20,000원 이상이면 종료되는지 테스트")
