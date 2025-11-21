c0f72162a715c6586c4b8f7592e77d75d18cc1bc
JunSeok_Park <pjh203203@naaver.com>
2025-11-21 11:19:43 +0900

    test: 게임이 시작되면 계속 게임이 돌아갈 수 있는 상태인지 테스트

--- src/test/java/arrangement/model/GameStateTest.java
+++ src/test/java/arrangement/model/GameStateTest.java
@@ -16,4 +16,12 @@ public class GameStateTest {
 
         assertThat(gameState.getState()).isFalse();
     }
+
+    @DisplayName("게임 생명 주기가 생성되면 게임 진행 가능한 상태인지 테스트")
+    @Test
+    void isCorrectGameStart() {
+        GameState gameState = new GameState();
+
+        assertThat(gameState.getState()).isTrue();
+    }
 }
