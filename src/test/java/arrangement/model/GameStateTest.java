c328f7b617fec9ff35b3ad0ac3accd3b5e4cb8dd
JunSeok_Park <pjh203203@naaver.com>
2025-11-21 11:31:42 +0900

    test: GameState 객체를 계속 생성하는 제거

--- src/test/java/arrangement/model/GameStateTest.java
+++ src/test/java/arrangement/model/GameStateTest.java
@@ -2,16 +2,21 @@ package arrangement.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class GameStateTest {
+    private GameState gameState;
+
+    @BeforeEach
+    void resetObject() {
+        gameState = new GameState();
+    }
 
     @DisplayName("종료 메시지를 보내면 올바르게 종료하는지 테스트")
     @Test
     void isCorrectFinish() {
-        GameState gameState = new GameState();
-
         gameState.off();
 
         assertThat(gameState.getState()).isFalse();
@@ -20,8 +25,6 @@ public class GameStateTest {
     @DisplayName("게임 생명 주기가 생성되면 게임 진행 가능한 상태인지 테스트")
     @Test
     void isCorrectGameStart() {
-        GameState gameState = new GameState();
-
         assertThat(gameState.getState()).isTrue();
     }
 }
